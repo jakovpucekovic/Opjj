c3ac42501993c687071550d745a616e51292dcac
jakovpucekovic <jakov.pucekovic@gmail.com>
2019-04-20 22:48:41 +0200

    [DZ-07] 4. zadatak

---
+++ hw07-1191239605/src/main/java/hr/fer/zemris/java/hw07/demo4/package-info.java
@@ -0,0 +1,9 @@
+/**
+ *	Package contains some examples of using the stream api for 
+ *	the seventh homework in the class Introduction to the Java
+ *	programming language held in the academic year 2018/2019.
+ *
+ * 	@author Jakov Pucekovic
+ *	@since 1.0
+ */
+package hr.fer.zemris.java.hw07.demo4;
