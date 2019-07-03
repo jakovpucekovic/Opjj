e6684e528af5af4c92a239ef6af06ea71f0f56ef
jakovpucekovic <jakov.pucekovic@gmail.com>
2019-07-03 12:40:18 +0200

    dodao 2. zadatak

---
+++ hw17-1191239605-2/src/main/java/hr/fer/zemris/java/hw17/jvdraw/JVDraw.java
@@ -0,0 +1,13 @@
+package hr.fer.zemris.java.hw17.jvdraw;
+
+/**
+ *	JVDraw TODO javadoc
+ * 
+ * 	@author Jakov Pucekovic
+ * 	@version 1.0
+ */
+public class JVDraw {
+
+	
+	
+}
---
+++ hw17-1191239605-2/src/main/java/hr/fer/zemris/java/hw17/jvdraw/package-info.java
@@ -0,0 +1,7 @@
+/**
+ *	package-info TODO javadoc
+ * 
+ * 	@author Jakov Pucekovic
+ * 	@version 1.0
+ */
+package hr.fer.zemris.java.hw17.jvdraw;
